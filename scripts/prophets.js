aec563664f11d925bfa2e3ebd2fd8f5dd11fa68c
jamesphillipdeguzman <jamesphillipdeguzman@gmail.com>
2024-10-02 11:31:23 +0800

    prophets v1.5 - Fixed layout and js when choosing a prophet.

--- scripts/prophets.js
+++ scripts/prophets.js
@@ -64,29 +64,18 @@ const dropdownProphets = (prophets) => {
 
 
         const stats = document.querySelector('#stats');
-        const p = document.createElement('p');
-        const prophetsStr = JSON.stringify(prophets[filter - 1], null, 2).replace(/\"/g, '').slice(1, -1).trim();
-        if (choice == 'all') {
-            // displayAllProphets(data.prophets);
-            // show in a nice format
 
-            console.log(prophetsStr);
-            const details = prophetsStr.split(',').map(item => item.trim()).join('\n');
-            stats.innerHTML = details;
+        if (choice === 'all') {
+
             getProphetData();
 
         }
         else {
             const filteredProphet = filterProphets(prophets, filter)
 
-            const details = prophetsStr.split(',').map(item => item.trim()).join('\n');
+
             stats.innerHTML = details;
         }
-
-        // p.setAttribute()
-
-        // const nameStr = `${prophetsStr[]}`;
-        // console.log(nameStr);
 
     });
 
@@ -98,33 +87,69 @@ const dropdownProphets = (prophets) => {
 
         option.value = completeName;
         option.textContent = completeName;
-        selector.appendChild(option);
+        selector.append(option);
 
     });
 
 }
 
+// ============== FILTER ====================
+
 const filterProphets = (prophets, filter) => {
-    // console.log(prophets[filter - 1].lastname);
 
-    // console.log(JSON.stringify(prophet[filter - 1]));
     cards.innerHTML = '';
-    stats.innerHTML = '';
 
-
+    // target card-wrapper class or element
+    const cardWrapper = document.querySelector('.card-wrapper');
+    // cardWrapper.className = 'grid';
+    cardWrapper.style.width = '1400px';
     // create a section element and define its class name
     const card = document.createElement('section');
-    card.className = 'card';
+    // card.className = 'grid';
+    card.style.maxWidth = '600px';
+
+    // target span tag with a class of stats
+    // const stats = document.querySelector('.stats');
+
+    const order = document.createElement('p');
+    const birthdate = document.createElement('p');
+    const birthplace = document.createElement('p');
+    const death = document.createElement('p');
+    const length = document.createElement('p');
+    const numofchildren = document.createElement('p');
+    const imageurl = document.createElement('a');
+
+    order.innerHTML = `<span class="label">Order:</span> ${prophets[filter - 1].order}`;
+    birthdate.innerHTML = `<span class="label">Birth:</span> ${prophets[filter - 1].birthdate}`;
+    birthplace.innerHTML = `<span class="label">Place:</span> ${prophets[filter - 1].birthplace}`;
+    death.innerHTML = `<span class="label">Death:</span> ${prophets[filter - 1].death}`;
+    length.innerHTML = `<span class="label">Length:</span> ${prophets[filter - 1].length} years`;
+    numofchildren.innerHTML = `<span class="label">Children:</span> ${prophets[filter - 1].numofchildren}`;
+    imageurl.innerHTML = `<span class="label">Picture:</span> <a href=${prophets[filter - 1].imageurl}>link</a>`;
+
     // create a h2 element and define its class name
     const fullName = document.createElement('h2');
     fullName.className = 'fullName';
+    fullName.style.fontSize = 'medium';
+    // fullName.style.margin = '0 auto';
+    fullName.style.maxWidth = '300px';
+    fullName.style.textAlign = 'left';
 
-    // create an img element and define its class name
-    const portrait = document.createElement('img');
-    portrait.className = 'portrait';
+    // create a span element
+    const stats = document.createElement('div');
+    stats.className = 'label';
+    stats.style.fontSize = '0.8rem';
+    stats.style.margin = '0 auto';
+    stats.style.maxWidth = '100%';
 
     // fullName is your h2 here...
     fullName.textContent = `${prophets[filter - 1].name} ${prophets[filter - 1].lastname}`;
+
+    // create an img element and define its class name
+    const portrait = document.createElement('img');
+    // portrait.className = 'grid';
+
+
 
     // set properties for your image element here...
     portrait.setAttribute('src', `${prophets[filter - 1].imageurl}`);
@@ -132,56 +157,120 @@ const filterProphets = (prophets, filter) => {
     portrait.setAttribute('loading', 'lazy');
     portrait.setAttribute('width', '200');
     portrait.setAttribute('height', '200');
+    portrait.style.border = '1px solid #ccc';
+    portrait.style.backgroundColor = '#000';
+    portrait.style.boxShadow = '0px 0px 4px #888';
 
+    // append all p tags to stats; then append stats and image to card
+    // append card to cards; finally append cards to cardWrapper
 
+    stats.append(fullName);
+    stats.append(order);
+    stats.append(birthplace);
+    stats.append(birthdate);
+    stats.append(death);
+    stats.append(length);
+    stats.append(numofchildren);
+    stats.append(imageurl);
 
-    // append fullName to card, portrait to card, and finally, your card to cards div
-    card.appendChild(fullName);
-    card.appendChild(portrait);
-    cards.appendChild(card);
+    card.append(stats);
+    card.append(portrait);
+
+    cards.append(card);
+
+    cardWrapper.append(cards);
+
 
 
 }
 
+// ============== DISPLAY ALL ====================
 
+const displayAllProphets = (prophets) => {
 
-const displayAllProphets = (prophets, filter) => {
+    cards.innerHTML = '';
 
     //card build code goes here
     prophets.forEach((prophet) => {
+        // target card-wrapper class or element
+        const cardWrapper = document.querySelector('.card-wrapper');
+        cardWrapper.classList.add('grid');
+        cardWrapper.style.width = '1400px';
         // create a section element and define its class name
         const card = document.createElement('section');
-        card.className = 'card';
-        card.style.maxWidth = '100%';
+        card.classList.add('grid');
+        card.style.maxWidth = '600px';
+
+        // target span tag with a class of stats
+        // const stats = document.querySelector('.stats');
+
+        const order = document.createElement('p');
+        const birthdate = document.createElement('p');
+        const birthplace = document.createElement('p');
+        const death = document.createElement('p');
+        const length = document.createElement('p');
+        const numofchildren = document.createElement('p');
+        const imageurl = document.createElement('a');
+
+        order.innerHTML = `<span class="label">Order:</span> ${prophet.order}`;
+        birthdate.innerHTML = `<span class="label">Birth:</span> ${prophet.birthdate}`;
+        birthplace.innerHTML = `<span class="label">Place:</span> ${prophet.birthplace}`;
+        death.innerHTML = `<span class="label">Death:</span> ${prophet.death}`;
+        length.innerHTML = `<span class="label">Length:</span> ${prophet.length} years`;
+        numofchildren.innerHTML = `<span class="label">Children:</span> ${prophet.numofchildren}`;
+        imageurl.innerHTML = `<span class="label">Picture:</span> <a href=${prophet.imageurl}>link</a>`;
 
         // create a h2 element and define its class name
         const fullName = document.createElement('h2');
         fullName.className = 'fullName';
-        fullName.style.fontSize = 'large';
-        fullName.style.margin = '0 auto';
-        fullName.style.width = '100%';
+        fullName.style.fontSize = 'medium';
+        // fullName.style.margin = '0 auto';
+        fullName.style.maxWidth = '300px';
+        fullName.style.textAlign = 'left';
 
-        // create an img element and define its class name
-        const portrait = document.createElement('img');
-        portrait.className = 'portrait';
+        // create a span element
+        const stats = document.createElement('div');
+        stats.className = 'label';
+        stats.style.fontSize = '0.8rem';
+        stats.style.margin = '0 auto';
+        stats.style.maxWidth = '100%';
 
         // fullName is your h2 here...
         fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+
+        // create an img element and define its class name
+        const portrait = document.createElement('img');
+        portrait.className = 'grid';
+
 
         // set properties for your image element here...
         portrait.setAttribute('src', `${prophet.imageurl}`);
         portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
         portrait.setAttribute('loading', 'lazy');
-        portrait.setAttribute('width', '300');
-        portrait.setAttribute('height', '300');
+        portrait.setAttribute('width', '200');
+        portrait.setAttribute('height', '200');
+        portrait.style.border = '1px solid #ccc';
+        portrait.style.backgroundColor = '#000';
+        portrait.style.boxShadow = '0px 0px 4px #888';
 
+        // append all p tags to stats; then append stats and image to card
+        // append card to cards; finally append cards to cardWrapper
 
+        stats.append(fullName);
+        stats.append(order);
+        stats.append(birthplace);
+        stats.append(birthdate);
+        stats.append(death);
+        stats.append(length);
+        stats.append(numofchildren);
+        stats.append(imageurl);
 
+        card.append(stats);
+        card.append(portrait);
 
-        // append fullName to card, portrait to card, and finally, your card to cards div
-        card.appendChild(fullName);
-        card.appendChild(portrait);
-        cards.appendChild(card);
+        cards.append(card);
+
+        cardWrapper.append(cards);
 
 
     });
