b754ddbbe7ac1fdc0746350e75954e864cb8b70e
jamesphillipdeguzman <jamesphillipdeguzman@gmail.com>
2024-10-15 01:08:27 +0800

    quote vitamins site page - fixed modals for desktop and mobile layouts.

--- web-project/scripts/base.js
+++ web-project/scripts/base.js
@@ -65,4 +65,147 @@ document.addEventListener('DOMContentLoaded', () => {
 
     }
 
+
+
+    // Get date and time format for timestamp
+
+    const now = new Date();
+    now.setMinutes(now.getMinutes() - now.getTimezoneOffset());
+    document.querySelector('#timestamp').value = now.toISOString().slice(0, 16);
+
+    // Target all modal elements
+    const modal = document.getElementById('modal');
+    const modalBackdrop = document.getElementById('modalBackdrop');
+    const closeModalBtns = document.getElementById('closeModal');
+    const submit = document.getElementById('submit');
+
+    // Have all modals display as none
+
+    modal.style.display = 'none';
+    modalBackdrop.style.display = 'none';
+
+
+
+    // Open modal
+    submit.addEventListener('click', () => {
+
+        // Target the form elements to display on Thank You modal
+
+        const firstname = document.querySelector('#firstname');
+        const lastname = document.querySelector('#lastname');
+
+        const email = document.querySelector('#email');
+        const mobile = document.querySelector('#mobile');
+
+
+        const timeStamp = document.querySelector('#timestamp');
+
+        // Target the li elements 
+        const fname = document.querySelector('#fname');
+        const lname = document.querySelector('#lname');
+        const emailAdd = document.querySelector('#email-add');
+        const phone = document.querySelector('#phone');
+
+        const dateTimeStamp = document.querySelector('#date-timestamp');
+
+
+        if (email.value != '' && mobile.value != '') {
+            modal.style.display = 'block';
+            modalBackdrop.style.display = 'block';
+
+            fname.textContent = `First Name: ${firstname.value}`;
+            lname.textContent = `Last Name: ${lastname.value}`;
+            emailAdd.textContent = `Email Address: ${email.value}`;
+            phone.textContent = `Contact Number: ${mobile.value}`;
+            dateTimeStamp.textContent = `Submitted: ${timeStamp.value}`;
+
+        }
+
+    });
+
+    // Close modal
+    closeModalBtns.addEventListener('click', () => {
+        modal.style.display = 'none';
+        modalBackdrop.style.display = 'none';
+    });
+
+    // Close modal when clicking on backdrop
+    modalBackdrop.addEventListener('click', () => {
+        modal.style.display = 'none';
+        modalBackdrop.style.display = 'none';
+    });
+
+
+
+
+
+
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+
+    // Target image layouts e.g. desktop
+
+    const modal1 = document.getElementById('modal1');
+    const modalBackdrop1 = document.getElementById('modalBackdrop1');
+    const closeModalBtns1 = document.getElementById('closeModal1');
+    const imgDesktopLayout = document.getElementById('img-desktop-layout');
+
+    modal1.style.display = 'none';
+    modalBackdrop1.style.display = 'none';
+
+    imgDesktopLayout.addEventListener('click', () => {
+        modal1.style.display = 'block';
+        modalBackdrop1.style.display = 'block';
+
+    });
+
+
+    // Close modal
+    closeModalBtns1.addEventListener('click', () => {
+        modal1.style.display = 'none';
+        modalBackdrop1.style.display = 'none';
+    });
+
+    // Close modal when clicking on backdrop
+    modalBackdrop1.addEventListener('click', () => {
+        modal1.style.display = 'none';
+        modalBackdrop1.style.display = 'none';
+    });
+
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Target image layouts e.g. mobile
+
+    const modal2 = document.getElementById('modal2');
+    const modalBackdrop2 = document.getElementById('modalBackdrop2');
+    const closeModalBtns2 = document.getElementById('closeModal2');
+    const imgMobileLayout = document.getElementById('img-mobile-layout');
+
+    modal2.style.display = 'none';
+    modalBackdrop2.style.display = 'none';
+
+    imgMobileLayout.addEventListener('click', () => {
+        modal2.style.display = 'block';
+        modalBackdrop2.style.display = 'block';
+
+    });
+
+
+    // Close modal
+    closeModalBtns2.addEventListener('click', () => {
+        modal2.style.display = 'none';
+        modalBackdrop2.style.display = 'none';
+    });
+
+    // Close modal when clicking on backdrop
+    modalBackdrop2.addEventListener('click', () => {
+        modal2.style.display = 'none';
+        modalBackdrop2.style.display = 'none';
+    });
+
 });
