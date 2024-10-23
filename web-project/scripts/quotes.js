828d98645bf2bfa69046b0f5fddc1e96fab98f0a
jamesphillipdeguzman <jamesphillipdeguzman@gmail.com>
2024-10-23 23:29:36 +0800

    added quotes.json

--- web-project/scripts/quotes.js
+++ web-project/scripts/quotes.js
@@ -1,9 +1,46 @@
-const api_url = "https://zenquotes.io/api/quotes/";
+// const api_url = "https://zenquotes.io/api/random";
 
-async function getapi(url) {
-    const response = await fetch(url);
-    var data = await response.json();
-    console.log(data);
+
+// async function getapi(api_url) {
+//     try {
+//         const response = await fetch(api_url);
+//         if (response.ok) {
+//             var data = await response.json();
+//             console.table(data);
+//         }
+//         else {
+//             throw new Error('No network response from server');
+//         }
+
+//     }
+//     catch (error) {
+//         console.error(error);
+//     }
+// }
+
+// getapi(api_url);
+
+
+const api_url = "data/quotes.json";
+
+
+async function getapi(api_url) {
+    try {
+        const response = await fetch(api_url);
+        if (response.ok) {
+            var data = await response.json();
+            console.table(data);
+        }
+        else {
+            throw new Error('No network response from server');
+        }
+
+    }
+    catch (error) {
+        console.error(error);
+    }
 }
 
 getapi(api_url);
+
+
